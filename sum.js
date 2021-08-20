19ae4cf71d88c9a6cdba5cd118f944c0ea27c18f
farhangani619 <farhangani694@gmail.com>
2021-08-20 11:01:54 +0530

    sum of n numbers implemented

--- sum.js
+++ sum.js
@@ -1,5 +1,10 @@
-function add(a,b){
-    return a+b
+function add(...args){
+    let sum = 0;
+
+    for (let i = 0; i < args.length; i++) {
+        sum += args[i];
+    }
+    return sum 
 }
-const sum = add(5,3)
+const sum = add(5,5,5,5,5)
 console.log(sum)
